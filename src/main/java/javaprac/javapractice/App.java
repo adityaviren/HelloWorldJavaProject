9773f20a313c28c7e05a0913103fb86b501b2b5f
adityaviren <adityaviren@gmail.com>
2020-09-21 10:23:15 +0530

    changes

--- src/main/java/javaprac/javapractice/App.java
+++ src/main/java/javaprac/javapractice/App.java
@@ -9,11 +9,14 @@ public class App
     public static void main( String[] args )
     {
     	Scanner sc=new Scanner(System.in);
+    	System.out.println("enter number 1");
     	int a=sc.nextInt();
+    	System.out.println("enter number 2");
         int b=sc.nextInt();
         add2numbers(a,b);
         multiply2numbers(a, b);
         minus2numbers(a, b);
+        rem2numbers(a, b);
         
     }
     private static void add2numbers(int a,int b) {
@@ -27,5 +30,9 @@ public class App
     private static void minus2numbers(int a,int b) {
     	long sum=a-b;
     	LOG.info("sum = "+sum);
+    }
+    private static void rem2numbers(int a,int b) {
+    	long rem=a%b;
+    	LOG.info("rem = "+rem);
     }
 }
